5214b268fd97d5657bcc7dc2dd08e96a334c025f
Afi <afimaamedufie@gmail.com>
2021-07-19 10:40:33 +0000

    minor comment

--- src/service/actions/actions.js
+++ src/service/actions/actions.js
@@ -17,6 +17,7 @@ import { message } from "antd";
 
 const BASE_URL = "http://localhost:8080/api/turntablexe/";
 
+//for prod
 // const BASE_URL = "https://turntablexebackend.herokuapp.com/api/turntablexe/";
 
 const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
